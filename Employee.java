f9e8d6b99061d1b56f6b7e1d8f666206bdb40f3c
Zandrex <zandrex@mail.ru>
2024-06-02 19:37:12 +0300

    git init

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package homework04;
+
+public class Employee {
+    private int tabel;
+    private int telNumber;
+    private String name;
+    private int experience;
+
+    public Employee(int tabel, int telNumber, String name, int experience) {
+        this.tabel = tabel;
+        this.telNumber = telNumber;
+        this.name = name;
+        this.experience = experience;
+    }
+}
